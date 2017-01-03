e518de17b990990e8f6bb5096289261eafa6d4db
WeiLunLo <b02703078>
2017-01-04 01:29:07 +0800

    fix done

--- testKTVcalculator/global.R
+++ testKTVcalculator/global.R
@@ -1,8 +1,8 @@
 MTWT <- "https://weilunlo.github.io/KTVcaculator/testKTVcalculator/boxMTWT.CSV"
-boxMTWT <- read.csv(MTWT,header = T, sep = ",")
+boxMTWT <- read.csv(MTWT,header = T, sep = ",",fileEncoding='big5')
 Fri <- "https://weilunlo.github.io/KTVcaculator/testKTVcalculator/boxFri.CSV"
-boxFri <- read.csv(Fri,header = T, sep = ",")
+boxFri <- read.csv(Fri,header = T, sep = ",",fileEncoding='big5')
 Sat <- "https://weilunlo.github.io/KTVcaculator/testKTVcalculator/boxSat.CSV"
-boxSat <- read.csv(Sat,header = T, sep = ",")
+boxSat <- read.csv(Sat,header = T, sep = ",",fileEncoding='big5')
 Sun <- "https://weilunlo.github.io/KTVcaculator/testKTVcalculator/boxSun.CSV"
-boxSun <- read.csv(Sun,header = T, sep = ",")
+boxSun <- read.csv(Sun,header = T, sep = ",",fileEncoding='big5')
