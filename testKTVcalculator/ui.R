bc790164b0d83f2666787aafde75af80d20a72dd
WeiLunLo <b02703078>
2017-01-07 23:23:28 +0800

    surface

--- testKTVcalculator/ui.R
+++ testKTVcalculator/ui.R
@@ -6,10 +6,10 @@ shinyUI(
     "KTV",
     tabPanel("KTV Calculator",
   fluidPage(
-            titlePanel("KTV計算機-測試版"),
+            titlePanel("KTV Calculator"),
   
     sidebarPanel(
-      helpText("請輸入唱歌資訊："),
+      h4("請輸入唱歌資訊：",style = "color:	#0072E3	"),
       
       selectInput("weekday", 
                   label = "唱歌的日子？",
@@ -20,7 +20,7 @@ shinyUI(
                   label = "人數？",
                   min = 1, max = 20, value = 7),
       
-      helpText("若人數超過20人請選20人，並建議聯絡客服人員詢問詳細收費方式"),
+      helpText("若人數超過20人請選20人，並建議聯絡客服人員詢問詳細收費方式",style = "color:	#02F78E	"),
 
       selectInput("start", 
                    label = "開始時間：", 
@@ -32,7 +32,7 @@ shinyUI(
                    choices = c(0:23),
                    selected = 0),
       helpText("24小時制，若『開始時間大於等於結束時間』則跨過一天。例：0→0 = 今天半夜唱到明天半夜；
-               23->2 = 今天晚上11點唱到明天凌晨2點"),
+               23->2 = 今天晚上11點唱到明天凌晨2點",style = "color:	#02F78E	"),
   
       checkboxGroupInput("discount", label = "等等！我有：(請至少選一項喔！)",
                    choices = list("一副好歌喉" = 1,"學生證" = 2, "威力卡" = 3, "晶鑽卡" = 4),
@@ -40,13 +40,14 @@ shinyUI(
   ),
 
     mainPanel(
-      h2("超級比一比"),
+      h2("包廂式計費",style = "color:	#0072E3	"),
       tableOutput("box"),
       div("*每人平均(元)：含餐飲最低基本消費與10%總金額之服務費。若要升級其他方案請上各KTV網站查詢詳情。",style = "color:grey"),
-      p("*建議包廂類型與人數對照表："),
-      tableOutput("boxname"),
       br(),
-      h2("目前資料與計價方式：三家之包廂",style = "color:red")
+      h4("建議包廂類型與人數對照表：",style = "color:	#0072E3	"),
+      tableOutput("boxname")
+      #br(),
+      #h2("目前資料與計價方式：三家之包廂",style = "color:red")
     )
   )),
   tabPanel("小柴",
